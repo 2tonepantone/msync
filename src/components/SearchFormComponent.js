80a468b909d7c2bb852bf8a6c72230c4181a3f6a
Kyle Bailey <kylealexbailey@gmail.com>
2022-02-21 22:30:34 +0900

    refine layout and styling

--- src/components/SearchFormComponent.js
+++ src/components/SearchFormComponent.js
@@ -1,38 +1,39 @@
 import React from "react";
 import { Form, Button } from "react-bootstrap"
-import FloatingLabel from 'react-bootstrap/FloatingLabel'
 
 const SearchFormComponent = () => {
   return (
     <Form>
       <Form.Group className="mb-3">
-        <div key={`inline-radio`}>
-          <Form.Check
-            inline
-            label="Similar Artists"
-            name="group1"
-            type="radio"
-            id={`inline-radio-artists`}
+        <Form.Check
+          inline
+          defaultChecked
+          label="Similar Artists"
+          name="group1"
+          type="radio"
+          id="radio-artists"
+        />
+        <Form.Check
+          inline
+          label="Similar Tracks"
+          name="group1"
+          type="radio"
+          id="radio-tracks"
+        />
+        <Form.Floating className="d-flex">
+          <Form.Control
+            required
+            id="floatingSearchInput"
+            type="text"
+            placeholder="Enter an artist or track name"
           />
-          <Form.Check
-            inline
-            label="Similar Tracks"
-            name="group1"
-            type="radio"
-            id={`inline-radio-tracks`}
-          />
-        </div>
-        <FloatingLabel
-          controlId="floatingInput"
-          label="Enter an artist or track name"
-        >
-          <Form.Control required type="text" placeholder="Enter an artist or track name" />
-        </FloatingLabel>
+          <label htmlFor="floatingSearchInput">Enter an artist or track name</label>
+          <Button variant="outline-primary" type="submit">Search</Button>
+        </Form.Floating>
         <Form.Text className="text-muted">
           Don't worry about spelling mistakes. We'll do our best to guess your intent.
         </Form.Text>
       </Form.Group>
-      <Button type="submit">Search</Button>
     </Form>
   )
 }
