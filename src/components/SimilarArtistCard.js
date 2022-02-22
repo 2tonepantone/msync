aa797966a95be2263f02cc467b5400f831998255
Kyle Bailey <kylealexbailey@gmail.com>
2022-02-22 18:29:45 +0900

    fetch and display relevant artist information

--- src/components/SimilarArtistCard.js
+++ src/components/SimilarArtistCard.js
@@ -1,7 +1,28 @@
-import React from "react"
-import Card from "react-bootstrap/card"
+import React, { useState, useEffect } from "react"
+import { Card, ListGroup, ListGroupItem } from "react-bootstrap"
 
-const SimilarArtistCard = ({ queriedArtist, name, match }) => {
+const SimilarArtistCard = ({ name, match, queriedArtist, mbid }) => {
+  const [listeners, setListeners] = useState()
+  const [playCount, setPlayCount] = useState()
+  const [tags, setTags] = useState()
+  const key = process.env.REACT_APP_KEY
+
+  useEffect(() =>{
+    fetch(`http://ws.audioscrobbler.com/2.0/?format=json&method=artist.getinfo&mbid=${mbid}&api_key=${key}`)
+      .then(res => res.json())
+      .then(
+        (result) => {
+          console.log("From card", result)
+          setListeners(parseInt(result.artist.stats.listeners).toLocaleString())
+          setPlayCount(parseInt(result.artist.stats.playcount).toLocaleString())
+          setTags(result.artist.tags.tag.map(tag => tag.name).join(', '))
+        },
+        (error) => {
+          console.log("Oops!", error)
+        }
+      )
+  }, [key, mbid])
+
   return (
     <Card style={{ width: '18rem' }}>
       <Card.Body>
@@ -10,6 +31,11 @@ const SimilarArtistCard = ({ queriedArtist, name, match }) => {
           {match}% similar to {queriedArtist}
         </Card.Subtitle>
       </Card.Body>
+      <ListGroup variant="flush">
+        {listeners && <ListGroupItem>Listeners: {listeners}</ListGroupItem>}
+        {playCount && <ListGroupItem>Play count: {playCount}</ListGroupItem>}
+        {tags && <ListGroupItem>Tags: {tags}</ListGroupItem>}
+      </ListGroup>
     </Card>
   )
 }
