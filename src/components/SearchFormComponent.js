c9d1eea7e946aec70202d08989ac200b979a4a5d
Kyle Bailey <kylealexbailey@gmail.com>
2022-02-22 16:02:54 +0900

    add api functionality and update form

--- src/components/SearchFormComponent.js
+++ src/components/SearchFormComponent.js
@@ -1,9 +1,39 @@
-import React from "react"
+import React, { useState } from "react"
 import { Form, Button } from "react-bootstrap"
 
 const SearchFormComponent = () => {
+  const [apiParams, setApiParams] = useState({similar: false, artist: "", track: "", tag: ""})
+  const key = process.env.REACT_APP_KEY
+
+  const handleChange = (event) => {
+    const { name, value } = event.target
+    setApiParams(prevApiParams => ({
+      ...prevApiParams,
+      [name]: value
+    }))
+  }
+
+  console.log(apiParams)
+  const handleClick = () => {
+    const { similar_tracks, artist, track, tag } = apiParams
+    const [encodedArtist, encodedTrack, encodedTag] = [ artist, track, tag ].map(str => encodeURIComponent(str))
+    console.log("Encoded", encodedArtist, encodedTrack, encodedTag)
+    const url = `http://ws.audioscrobbler.com/2.0/?method=artist.getsimilar&artist=${encodedArtist}&limit=10&api_key=${key}&format=json`
+    console.log(url);
+    fetch(url)
+      .then(res => res.json())
+      .then(
+        (result) => {
+          console.log(result)
+        },
+        (error) => {
+          console.log("Oops!", error)
+        }
+      )
+  }
+
   return (
-    <Form>
+    <Form onSubmit={(e) => e.preventDefault()}>
       <Form.Group>
         <Form.Check
           inline
@@ -12,6 +42,7 @@ const SearchFormComponent = () => {
           name="similar"
           value="artists"
           type="radio"
+          onChange={handleChange}
         />
         <Form.Check
           inline
@@ -19,6 +50,15 @@ const SearchFormComponent = () => {
           name="similar"
           value="tracks"
           type="radio"
+          onChange={handleChange}
+        />
+        <Form.Check
+          inline
+          label="Similar Tags"
+          name="similar"
+          value="tags"
+          type="radio"
+          onChange={handleChange}
         />
       </Form.Group>
       <Form.Group>
@@ -28,10 +68,17 @@ const SearchFormComponent = () => {
             id="floatingSearchInput"
             type="text"
             placeholder="Enter an artist or track name"
-            name="query"
-          />
+            name="artist"
+            onChange={handleChange}
+            />
           <label htmlFor="floatingSearchInput">Enter an artist or track name</label>
-          <Button variant="outline-primary" type="submit">Search</Button>
+          <Button
+            variant="outline-primary"
+            type="submit"
+            onClick={handleClick}
+          >
+            Search
+          </Button>
         </Form.Floating>
         <Form.Text className="text-muted">
           Don't worry about spelling mistakes. We'll do our best to guess your intent.
