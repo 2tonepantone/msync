c5d49ca23729268de9b0b7ec9eef949061e30bdf
Kyle Bailey <kylealexbailey@gmail.com>
2022-02-24 19:16:44 +0900

    add top albums and tracks

--- src/routes/ArtistDetail.js
+++ src/routes/ArtistDetail.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react"
-import { Card, ListGroup, ListGroupItem, Container, Row, Col } from "react-bootstrap"
+import { Card, ListGroup, ListGroupItem, Container, Row, Col, Accordion } from "react-bootstrap"
 import { useSelector } from "react-redux"
 import { useParams } from "react-router-dom"
 import SimilarArtistCard from "../components/SimilarArtistCard"
@@ -9,9 +9,35 @@ const ArtistDetail = () => {
   const key = process.env.REACT_APP_KEY
   const { artist } = useParams()
   const artistsData = useSelector(state => state.artists)
+  const [topTracks, setTopTracks] = useState()
+  const [topAlbums, setTopAlbums] = useState()
   // console.log(`From detail ${Object.keys(artistsData).length}`, artistsData)
   const { name, bio, ontour, similar, stats, tags } = artistsData[artist]
-  console.log("artist")
+
+  useEffect(() => {
+    fetch(`http://ws.audioscrobbler.com/2.0/?method=artist.gettoptracks&artist=${artist}&api_key=${key}&format=json&limit=10`)
+      .then(res => res.json())
+      .then(
+        (result) => {
+          setTopTracks(result.toptracks.track)
+        },
+        (error) => {
+          console.log("Oops!", error)
+        }
+      )
+
+    fetch(`http://ws.audioscrobbler.com/2.0/?method=artist.gettopalbums&artist=${artist}&api_key=${key}&format=json`)
+      .then(res => res.json())
+      .then(
+        (result) => {
+          setTopAlbums(result.topalbums.album)
+        },
+        (error) => {
+          console.log("Oops!", error)
+        }
+      )
+  }, [artist, key])
+
   return (
     <Container className="m-5">
       <Card>
@@ -21,10 +47,41 @@ const ArtistDetail = () => {
             {ontour === '1' ? "Currently" : "Not"} touring
           </Card.Subtitle>
           <Card.Text>
-            {bio.content}
+            {bio.summary}
           </Card.Text>
         </Card.Body>
         <ListGroup variant="flush">
+          <Accordion>
+            <Accordion.Item eventKey="0">
+              <Accordion.Header>Top Tracks:</Accordion.Header>
+              <Accordion.Body>
+              {topTracks && topTracks.map((track, i) => (
+                <>
+                  <Card.Header>{`${i+1}. ${track.name}`}</Card.Header>
+                  <ListGroupItem>
+                    Plays: {parseInt(track.playcount).toLocaleString()}, Listeners: {parseInt(track.listeners).toLocaleString()}
+                  </ListGroupItem>
+                </>
+              ))}
+              </Accordion.Body>
+            </Accordion.Item>
+          </Accordion>
+          <Accordion>
+            <Accordion.Item eventKey="0">
+              <Accordion.Header>Top Albums:</Accordion.Header>
+              <Accordion.Body>
+                {console.log("Photos", topAlbums)}
+              {topAlbums && topAlbums.slice(0,10).map((album, i) => (
+                <>
+                  <Card.Header>{`${i+1}. ${album.name}`}</Card.Header>
+                  <ListGroupItem>
+                    Plays: {parseInt(album.playcount).toLocaleString()}
+                  </ListGroupItem>
+                </>
+              ))}
+              </Accordion.Body>
+            </Accordion.Item>
+          </Accordion>
           <ListGroupItem>Listeners: {parseInt(stats.listeners).toLocaleString()}</ListGroupItem>
           <ListGroupItem>Play count: {parseInt(stats.playcount).toLocaleString()}</ListGroupItem>
           <ListGroupItem>Tags: {(tags.tag).map(tag => tag.name).join(', ')}</ListGroupItem>
@@ -32,15 +89,15 @@ const ArtistDetail = () => {
       </Card>
       <Row className="mt-5">
         {similar.artist && <h5>Similar Artists:</h5>}
-        {similar.artist && similar.artist.map(artist => (
-          <Col className="g-4">
-            <SimilarArtistCard
-              name={artist.name}
-              queriedArtist={name}
-              key={artist.mbid}
-              mbid={artist.mbid}
-            />
-          </Col>
+        {similar.artist && similar.artist.slice(0,3).map(artist => (
+            <Col className="g-4">
+              <SimilarArtistCard
+                name={artist.name}
+                queriedArtist={name}
+                key={artist.mbid}
+                mbid={artist.mbid}
+              />
+            </Col>
         ))}
       </Row>
     </Container>
