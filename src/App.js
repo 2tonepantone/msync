1495346315a7d0877593be354e17922721235464
Kyle Bailey <kylealexbailey@gmail.com>
2022-02-25 17:16:55 +0900

    remove redundancies

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
-import SearchFormComponent from './components/SearchFormComponent'
-import NavbarComponent from './components/NavbarComponent';
+import NavbarComponent from './components/NavbarComponent'
 import './App.css'
-import { Outlet } from 'react-router-dom';
+import { Outlet } from 'react-router-dom'
 
 function App() {
   return (
