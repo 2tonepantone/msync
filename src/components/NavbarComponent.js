f36132641efb95a5fc35d03c1e9e98a2ef26d000
Kyle Bailey <kylealexbailey@gmail.com>
2022-02-25 14:49:15 +0900

    use bootstrap element

--- src/components/NavbarComponent.js
+++ src/components/NavbarComponent.js
@@ -1,11 +1,11 @@
 import React from "react"
-import { Container } from "react-bootstrap"
+import { Container, Nav } from "react-bootstrap"
 import SearchFormComponent from "./SearchFormComponent"
 import { Link } from "react-router-dom"
 
 export default function NavbarComponent() {
   return (
-    <nav class="navbar navbar-light bg-light">
+    <Nav className="navbar navbar-light bg-light">
       <Container>
         <span class="navbar-brand">MSYNC</span>
         <Link to="/">Home</Link>
@@ -13,6 +13,6 @@ export default function NavbarComponent() {
         <Link to="#">Saved Tracks</Link>
         <SearchFormComponent />
       </Container>
-    </nav>
+    </Nav>
   )
 }
