f13d9642a0f46a9362b72fd23d33c666dbda8b2a
Kyle Bailey <kylealexbailey@gmail.com>
2022-02-25 19:47:11 +0900

    implement rough listsSlice

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit'
 import artistReducer from '../features/artists/artistsSlice'
+import listReducer from '../features/artists/listsSlice'
 
 export default configureStore({
   reducer: {
     artists: artistReducer,
+    lists: listReducer,
   },
 })
