d9e481c1e9826cbb2aaa4f15895c48a48e9c56db
Kyle Bailey <kylealexbailey@gmail.com>
2022-02-25 15:33:31 +0900

    add handleClick

--- src/components/NavbarComponent.js
+++ src/components/NavbarComponent.js
@@ -4,11 +4,18 @@ import SearchFormComponent from "./SearchFormComponent"
 import { Link } from "react-router-dom"
 
 export default function NavbarComponent() {
+  const handleClick = () => {
+    const input = document.getElementById('artistSearchInput')
+    console.log("Input:", input.value)
+    input.value = ''
+    console.log("Input:", input.value)
+  }
+
   return (
     <Nav className="navbar navbar-light bg-light">
       <Container>
         <span className="navbar-brand">MSYNC</span>
-        <Link to="/">Home</Link>
+        <Link to="/" onClick={handleClick}>Home</Link>
         <Link to="#">Saved Artists</Link>
         <Link to="#">Saved Tracks</Link>
         <SearchFormComponent />
