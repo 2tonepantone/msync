7af34087c34976b65788f40b4786aea8d6e6493f
Kyle Bailey <kylealexbailey@gmail.com>
2022-02-25 20:13:25 +0900

    add favorite button

--- src/components/SimilarArtistCard.js
+++ src/components/SimilarArtistCard.js
@@ -1,14 +1,16 @@
 import React, { useState, useEffect } from "react"
-import { Card, ListGroup, ListGroupItem } from "react-bootstrap"
-import { useDispatch } from "react-redux"
+import { Button, Card, ListGroup, ListGroupItem } from "react-bootstrap"
+import { useDispatch, useSelector } from "react-redux"
 import { Link } from "react-router-dom"
 import { addArtist } from "../features/artists/artistsSlice"
+import { addItem } from "../features/artists/listsSlice"
 
 const SimilarArtistCard = ({ name, match, queriedArtist, mbid }) => {
   const [listeners, setListeners] = useState()
   const [playCount, setPlayCount] = useState()
   const [tags, setTags] = useState()
   const key = process.env.REACT_APP_KEY
+  const artistsData = useSelector(state => state.artists)
   const dispatch = useDispatch()
 
   useEffect(() =>{
@@ -29,11 +31,20 @@ const SimilarArtistCard = ({ name, match, queriedArtist, mbid }) => {
   }, [mbid, name])
 
   const handleClick = () => document.getElementById('artistSearchInput').value = ''
+  const artistData = { [name]: artistsData[name] }
 
   return (
     <Card style={{ width: '18rem' }}>
       <Card.Body>
-        <Card.Title>{name}</Card.Title>
+        <Card.Title className="d-flex justify-content-between">
+          {name}
+          <Button
+            size="sm"
+            onClick={() => dispatch(addItem(artistData))}
+          >
+            Favorite
+          </Button>
+        </Card.Title>
         <Card.Subtitle className="mb-2 text-muted">
           {match && `${match}% similar to ${queriedArtist}`}
         </Card.Subtitle>
