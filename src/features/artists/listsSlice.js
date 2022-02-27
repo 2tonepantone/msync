b8d554e92128e3247442fa437580bb92bec6819c
Kyle Bailey <kylealexbailey@gmail.com>
2022-02-27 23:19:21 +0900

    add deleteList reducer

--- src/features/artists/listsSlice.js
+++ src/features/artists/listsSlice.js
@@ -11,8 +11,12 @@ export const listsSlice = createSlice({
       const filteredList = state.filter(obj => obj.items.name !== action.payload)
       return [ ...filteredList ]
     },
+    deleteList: (state, action) => {
+      const filteredLists = state.filter(obj => obj.listTitle !== action.payload)
+      return [...filteredLists]
+    }
   },
 })
 
-export const { addItem, deleteItem } = listsSlice.actions
+export const { addItem, deleteItem, deleteList } = listsSlice.actions
 export default listsSlice.reducer
