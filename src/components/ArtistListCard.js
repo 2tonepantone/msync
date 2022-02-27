7a80dd32859a983be0db3ba36eb2979d981e8939
Kyle Bailey <kylealexbailey@gmail.com>
2022-02-28 03:00:12 +0900

    remove import

--- src/components/ArtistListCard.js
+++ src/components/ArtistListCard.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { Accordion, Button, Card, ListGroup, ListGroupItem } from "react-bootstrap"
+import { Accordion, Card, ListGroup, ListGroupItem } from "react-bootstrap"
 import { Link } from "react-router-dom"
 import DeleteButton from "./DeleteButton"
 
