6c382fa5c754142ec1e4eb1f10da0523a906d32d
Kyle Bailey <kylealexbailey@gmail.com>
2022-02-28 00:02:07 +0900

    add track search

--- src/components/SearchFormComponent.js
+++ src/components/SearchFormComponent.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react"
 import { useNavigate } from "react-router-dom"
-import { Form, Button } from "react-bootstrap"
+import { Form, Button, Row, Col } from "react-bootstrap"
 
 const SearchFormComponent = () => {
   const [artistQuery, setArtistQuery] = useState()
@@ -13,7 +13,15 @@ const SearchFormComponent = () => {
       var timeOutId = setTimeout(() => navigate(`artists/${artistQuery}`), 500)
     }
 
-    return () => clearTimeout(timeOutId)
+    if (artistQuery && trackQuery) {
+      var timeOutIdTracks = setTimeout(() => navigate(`artists/${artistQuery}`), 500)
+    }
+
+    return () => {
+      clearTimeout(timeOutId)
+      clearTimeout(timeOutIdTracks)
+    }
+
   }, [artistQuery])
 
   const handleSubmit = (e) => {
@@ -31,6 +39,7 @@ const SearchFormComponent = () => {
           name="similar"
           value="artists"
           type="radio"
+          onClick={() => setSimilarTracks(false)}
         />
         <Form.Check
           inline
@@ -38,29 +47,59 @@ const SearchFormComponent = () => {
           name="similar"
           value="tracks"
           type="radio"
+          onClick={() => setSimilarTracks(true)}
         />
       </Form.Group>
       <Form.Group>
-        <Form.Floating className="d-flex">
-          <Form.Control
-            required
-            id="artistSearchInput"
-            type="text"
-            placeholder="Enter an artist name"
-            name="artist"
-            onChange={e => setArtistQuery(e.target.value)}
-            />
-          <label htmlFor="artistSearchInput">Enter an artist's name</label>
-          <Button
-            variant="outline-primary"
-            type="submit"
-          >
-            Search
-          </Button>
-        </Form.Floating>
-        <Form.Text className="text-muted">
+        <Row>
+          <Col>
+            <Form.Floating className="d-flex">
+              <Form.Control
+                required
+                id="artistSearchInput"
+                type="text"
+                size="sm"
+                name="artist"
+                placeholder="Enter an artist"
+                onChange={e => setArtistQuery(e.target.value)}
+              />
+              <label htmlFor="artistSearchInput">Enter an artist</label>
+              {!similarTracks &&
+                <Button
+                  variant="outline-primary"
+                  type="submit"
+                >
+                  Search
+                </Button>
+              }
+            </Form.Floating>
+          </Col>
+        {similarTracks &&
+          <Col>
+            <Form.Floating className="d-flex">
+              <Form.Control
+                required
+                id="trackSearchInput"
+                type="text"
+                size="sm"
+                name="track"
+                placeholder="Enter a track"
+                onChange={e => setTrackQuery(e.target.value)}
+              />
+              <label htmlFor="trackSearchInput">Enter a track</label>
+              <Button
+                variant="outline-primary"
+                type="submit"
+              >
+                Search
+              </Button>
+            </Form.Floating>
+          </Col>
+        }
+        </Row>
+        {/* <Form.Text className="text-muted">
           Don't worry about spelling mistakes. We'll do our best to guess your intent.
-        </Form.Text>
+        </Form.Text> */}
       </Form.Group>
     </Form>
   )
