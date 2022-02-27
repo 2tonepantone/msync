0bc0ac74b4c4a558e333c87da3925e8a5347e7e9
Kyle Bailey <kylealexbailey@gmail.com>
2022-02-28 03:12:14 +0900

    update to accept artists and tracks lists

--- src/components/DeleteButton.js
+++ src/components/DeleteButton.js
@@ -2,12 +2,17 @@ import React from "react"
 import { Button } from "react-bootstrap"
 import { useDispatch } from "react-redux"
 import { deleteItem, deleteList } from "../features/artists/listsSlice"
+import { deleteTrackItem, deleteTrackList } from "../features/tracks/trackListsSlice"
 
-const DeleteButton = ({ target, targetType }) => {
+const DeleteButton = ({ target, targetType, listType }) => {
   const dispatch = useDispatch()
 
   const handleDelete = () => {
-    targetType === 'list' ? dispatch(deleteList(target)) : dispatch(deleteItem(target))
+    if (listType === 'artists') {
+      targetType === 'list' ? dispatch(deleteList(target)) : dispatch(deleteItem(target))
+    } else {
+      targetType === 'list' ? dispatch(deleteTrackList(target)) : dispatch(deleteTrackItem(target))
+    }
   }
 
   return (
