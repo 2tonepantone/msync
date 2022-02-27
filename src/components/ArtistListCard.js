97837f578a1800ac8872e80a48bfec9d92c69232
Kyle Bailey <kylealexbailey@gmail.com>
2022-02-27 14:33:46 +0900

    update object access

--- src/components/ArtistListCard.js
+++ src/components/ArtistListCard.js
@@ -2,17 +2,17 @@ import React from "react"
 import { Accordion, Card, ListGroup, ListGroupItem } from "react-bootstrap"
 import { Link } from "react-router-dom"
 
-const ArtistListCard = ({artistsData}) => {
+const ArtistListCard = ({listTitle, artistsData}) => {
   return (
     <Card>
       <Card.Body>
-        <Card.Title>Favorite Artists</Card.Title>
+        <Card.Title>{listTitle}</Card.Title>
         <Card.Subtitle className="mb-2 text-muted">
-          {artistsData.length} item(s) in list
+          {artistsData.items.length} item(s) in list
         </Card.Subtitle>
       </Card.Body>
       <ListGroup variant="flush">
-        {artistsData && artistsData.map((artist, i) => (
+        {artistsData && artistsData.items.map((artist, i) => (
           <Accordion>
             <Accordion.Item eventKey={artist.name}>
               <Accordion.Header><strong>{i + 1}. {artist.name}</strong></Accordion.Header>
