817e4729c3b3bdcd675e2bb8d8cb0e6021992c42
Kyle Bailey <kylealexbailey@gmail.com>
2022-02-27 22:50:28 +0900

    add functionality to delete artists from lists

--- src/components/ArtistListCard.js
+++ src/components/ArtistListCard.js
@@ -1,18 +1,26 @@
 import React from "react"
-import { Accordion, Card, ListGroup, ListGroupItem } from "react-bootstrap"
+import { Accordion, Button, Card, ListGroup, ListGroupItem } from "react-bootstrap"
 import { Link } from "react-router-dom"
+import { useDispatch } from "react-redux"
+import { deleteItem } from "../features/artists/listsSlice"
 
-const ArtistListCard = ({listTitle, artistsData}) => {
+const ArtistListCard = ({ listTitle, artistList }) => {
+  const dispatch = useDispatch()
+
+  const handleDelete = (artist) => {
+    dispatch(deleteItem(artist))
+  }
+
   return (
     <Card style={{ width: '18rem' }}>
       <Card.Body>
         <Card.Title>{listTitle}</Card.Title>
         <Card.Subtitle className="mb-2 text-muted">
-          {artistsData.items.length} item(s) in list
+          {artistList.items.length} item(s) in list
         </Card.Subtitle>
       </Card.Body>
       <ListGroup variant="flush">
-        {artistsData && artistsData.items.map((artist, i) => (
+        {artistList.items.map((artist, i) => (
           <Accordion>
             <Accordion.Item eventKey={artist.name}>
               <Accordion.Header><strong>{i + 1}. {artist.name}</strong></Accordion.Header>
@@ -20,8 +28,15 @@ const ArtistListCard = ({listTitle, artistsData}) => {
                 <ListGroupItem>Play count: {parseInt(artist.stats.playcount).toLocaleString()}</ListGroupItem>
                 <ListGroupItem>Listeners: {parseInt(artist.stats.listeners).toLocaleString()}</ListGroupItem>
                 <ListGroupItem>Tags: {(artist.tags.tag).map(tag => tag.name).join(', ')}</ListGroupItem>
-                <ListGroupItem>
+                <ListGroupItem className="d-flex justify-content-between">
                   <Link to={`/artist/${artist.name}`}>More info</Link>
+                  <Button
+                    size="sm"
+                    variant="outline-danger"
+                    onClick={() => handleDelete(artist.name)}
+                  >
+                    Delete
+                  </Button>
                 </ListGroupItem>
               </Accordion.Body>
             </Accordion.Item>
